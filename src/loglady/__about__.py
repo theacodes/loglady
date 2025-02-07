da28e998c675594f32fbf3f74f36e291ca8481f7
Thea Flowers <thea@winterbloom.com>
2025-02-07 15:21:39 -0500

    Bump version to 2025.2.7

--- src/loglady/__about__.py
+++ src/loglady/__about__.py
@@ -2,6 +2,6 @@
 # Published under the standard MIT License.
 # Full text available at: https://opensource.org/licenses/MIT
 
-__version__ = "2024.11.29"
+__version__ = "2025.2.7"
 __author__ = "Alethea Katherine Flowers"
 __author_email__ = "me@thea.codes"
