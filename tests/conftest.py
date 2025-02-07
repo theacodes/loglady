ba7776314013382c592fc2131d3d75728d8473bc
Thea Flowers <thea@winterbloom.com>
2025-02-07 12:32:32 -0500

    Fixed pytest plugin being loaded twice in tests

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2024 Alethea Katherine Flowers.
-# Published under the standard MIT License.
-# Full text available at: https://opensource.org/licenses/MIT
-
-pytest_plugins = ["loglady.pytest"]
