a1cd013a5b39d42e5f4497aac4a62c7570830447
Thea Flowers <thea@winterbloom.com>
2025-07-21 19:03:14 -0400

    Updated `test_processors.py` to remove tests related to now-removed processors for exceptions and stack traces.

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -3,45 +3,19 @@
 # Full text available at: https://opensource.org/licenses/MIT
 
 
-from types import FrameType
-
 import loglady.processors
-from loglady.types import SysExcInfo
-
-
-def test_add_stack_info():
-    record = loglady.processors.add_exception_and_stack_info(dict(stack_info=True))
-
-    stack: FrameType | None = record.get("stacktrace")
-    assert stack is not None
-    assert stack.f_globals.get("__file__") == __file__
-
-
-def test_add_exception_info():
-    record = dict(exc_info=True)
-    err = RuntimeError()
-
-    try:
-        raise err
-    except RuntimeError:
-        record = loglady.processors.add_exception_and_stack_info(record)
-
-    excinfo: SysExcInfo | None = record.get("exception")
-    assert excinfo is not None
-
-    exc_type, exc, traceback = excinfo
-
-    assert exc_type is RuntimeError
-    assert exc == err
-    assert traceback is not None
+from loglady.exception_capture import CapturedFrame
+from loglady.types import ReservedKeys
 
 
 def test_add_call_info():
     record = loglady.processors.add_call_info(dict())
 
-    assert record.get("call_fn") == "test_add_call_info"
-    assert record.get("call_filename") == __file__
-    assert record.get("call_module") == __name__
+    info = record.get(ReservedKeys.captured_call_info)
+    assert isinstance(info, CapturedFrame)
+    assert info.qualname == "test_add_call_info"
+    assert info.module_name == __name__
+    assert info.filename == __file__
 
 
 def test_add_call_info_with_invisible_fn():
@@ -51,6 +25,8 @@ def test_add_call_info_with_invisible_fn():
 
     record = invisible_fn()
 
-    assert record.get("call_fn") == "test_add_call_info_with_invisible_fn"
-    assert record.get("call_filename") == __file__
-    assert record.get("call_module") == __name__
+    info = record.get(ReservedKeys.captured_call_info)
+    assert isinstance(info, CapturedFrame)
+    assert info.qualname == "test_add_call_info_with_invisible_fn"
+    assert info.module_name == __name__
+    assert info.filename == __file__
